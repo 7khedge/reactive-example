7b1570d7ec830be45b3119865041a358861f7900
7khedge <info@7khedge.com>
2016-02-08 14:09:22 +0000

    Hash

--- sf-util/src/main/java/com/sf/util/security/HashUtil.java
+++ sf-util/src/main/java/com/sf/util/security/HashUtil.java
@@ -1,6 +1,7 @@
 package com.sf.util.security;
 
-import java.io.UnsupportedEncodingException;
+import com.google.common.base.Charsets;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -8,31 +9,27 @@ import java.security.NoSuchAlgorithmException;
  * Created by adityasofat on 08/02/2016.
  */
 public class HashUtil {
-    private  MessageDigest digest;
-    private  final char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+    private final char[] hex = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    public static final String sha1 = "SHA-1";
+    public static final String md5 = "MD5";
 
-    public void init(){
+    public String hash(String string, String algorithm) {
+        MessageDigest digest = null;
         try {
-            digest = MessageDigest.getInstance("SHA-256");
+            digest = MessageDigest.getInstance(algorithm);
         } catch (NoSuchAlgorithmException e) {
             throw new SecurityException(e);
         }
+        return byteArray2Hex(digest.digest(string.getBytes(Charsets.UTF_8)));
+
     }
 
     private String byteArray2Hex(byte[] bytes) {
         StringBuffer sb = new StringBuffer(bytes.length * 2);
-        for(final byte b : bytes) {
+        for (final byte b : bytes) {
             sb.append(hex[(b & 0xF0) >> 4]);
             sb.append(hex[b & 0x0F]);
         }
         return sb.toString();
-    }
-
-    public String hash(String string){
-        try {
-            return byteArray2Hex(digest.digest(string.getBytes("UTF-8")));
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
--- sf-util/src/test/java/com/sf/util/security/HashUtilShould.java
+++ sf-util/src/test/java/com/sf/util/security/HashUtilShould.java
@@ -1,19 +1,54 @@
 package com.sf.util.security;
 
+import com.google.common.base.Charsets;
+import com.google.common.hash.Hashing;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
 /**
- * Created by adityasofat on 08/02/2016.
+ * test to determine hash choice and ensure validate hash produce
  */
 public class HashUtilShould {
+
+    private String simpleString = "HelloWorld";
+    /* 160 bit, 40 characters*/
+    private String simpleStringSha1Hash = "db8ac1c259eb89d4a131b253bacfca5f319d54f2";
+    /* 128 bit, 32 characters*/
+    private String simpleStringMD5Hash = "68e109f0f40ca72a15e05cc22786f8e6";
+
+    private String jsonString = "{\"id\":1,\"name\":\"GCD\",\"server\":\"lonlx102394\",\"port\":\"1023\",\"instanceName\":\"ms1\",\"Component\":\"restService\"}";
+    /* 160 bit, 40 characters*/
+    private String jsonStringSha1Hash = "0d22e2b600a94f7dd99c6bf98a28e61bb7c96ee2";
+    /* 128 bit, 32 characters*/
+    private String jsonStringMD5Hash = "1e049636ce26aa66c4829d055a18c0e9";
+
+
     @Test
-    public void shouldGenerateHash() {
-        String expectedHash = "5e32599a52d03d8c901136650dfbbf37f42b205ddba02c194cf58d61477f48b5";
-        String stringToHash = "{\"id\":1,\"name\":\"GCD\",\"server\":\"lonlx102394\",\"port\":\"1023\",\"instanceName\":\"ms1\",\"Component\":\"restService\"}";
-        HashUtil hashUtil = new HashUtil();
-        hashUtil.init();
-        MatcherAssert.assertThat(hashUtil.hash(stringToHash), CoreMatchers.equalTo(expectedHash));
+    public void shouldGenerateSHA1Hash() {
+        MatcherAssert.assertThat(new HashUtil().hash(simpleString,HashUtil.sha1), CoreMatchers.equalTo(simpleStringSha1Hash));
+        MatcherAssert.assertThat(new HashUtil().hash(jsonString,HashUtil.sha1), CoreMatchers.equalTo(jsonStringSha1Hash));
     }
+
+    @Test
+    public void shouldGenerateSHA1HashingWithGuava() {
+        MatcherAssert.assertThat(Hashing.sha1().hashString(simpleString, Charsets.UTF_8).toString(), CoreMatchers.equalTo(simpleStringSha1Hash));
+        MatcherAssert.assertThat(Hashing.sha1().hashString(jsonString, Charsets.UTF_8).toString(), CoreMatchers.equalTo(jsonStringSha1Hash));
+    }
+
+
+    @Test
+    public void shouldGenerateMD5Hash() {
+        MatcherAssert.assertThat(new HashUtil().hash(simpleString,HashUtil.md5), CoreMatchers.equalTo(simpleStringMD5Hash));
+        MatcherAssert.assertThat(new HashUtil().hash(jsonString,HashUtil.md5), CoreMatchers.equalTo(jsonStringMD5Hash));
+    }
+
+
+    /*Chosen to use md5 using Guava*/
+    @Test
+    public void shouldGenerateMD5HashingWithGuava() {
+        MatcherAssert.assertThat(Hashing.md5().hashString(simpleString, Charsets.UTF_8).toString(), CoreMatchers.equalTo(simpleStringMD5Hash));
+        MatcherAssert.assertThat(Hashing.md5().hashString(jsonString, Charsets.UTF_8).toString(), CoreMatchers.equalTo(jsonStringMD5Hash));
+    }
+
 }
